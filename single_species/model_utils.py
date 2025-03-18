bb4601845a285a811177b45110578d4c73e36edd
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2025-03-18 21:37:41 +0200

    Add single_species subdirectory

---
+++ single_species/model_utils.py
@@ -0,0 +1,95 @@
+import tqdm
+import numpy as np
+import torch
+from torch.distributions.normal import Normal
+from scipy.stats import norm as scipy_norm
+
+#########################################################################################
+# DETECTION MODEL
+#########################################################################################
+
+def detection_loss(beta, y, X, threshold, beta_mean, beta_prec):
+    L = X@beta
+    Phi_L = Normal(0, 1).cdf(L)*threshold
+    Phi_L = torch.clamp(Phi_L, 1e-6, 1 - 1e-6)
+    llh = torch.mean(y*torch.log(Phi_L) + (1 - y)*torch.log(1 - Phi_L))
+    penalty = -0.5*beta_prec*torch.sum((beta - beta_mean)**2)
+    return -(llh + penalty)
+
+def fit_detection_model(y_train, X_train, threshold_train, beta_mean, beta_prec,
+                       lr=0.1, num_epochs=10000, tol=0.0001):
+    y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+    threshold_train_tensor = torch.tensor(threshold_train, dtype=torch.float32)
+    beta_mean_tensor = torch.tensor(beta_mean, dtype=torch.float32)
+    
+    beta = torch.tensor([-3.0, -3.0, 1.0, 0.0, 0.0], requires_grad=True)
+    optimizer = torch.optim.Adam([beta], lr=lr)
+    losses = []
+    for epoch in tqdm.tqdm(range(num_epochs), desc="Training detection model"):
+        optimizer.zero_grad()
+        loss = detection_loss(beta, y_train_tensor, X_train_tensor, threshold_train_tensor, beta_mean_tensor, beta_prec)
+        loss.backward()
+        optimizer.step()
+
+        if (epoch + 1) % 100 == 0 or epoch == 0:
+            losses.append(loss.item())
+
+        if len(losses)>2 and np.abs(losses[-1] - losses[-2])/losses[-2] < tol:
+            break
+    
+    return beta.detach().numpy()
+
+
+#########################################################################################
+# MIGRATION MODEL
+#########################################################################################
+
+def m_numpy(lats, days, theta):
+    Z1 = (days - (theta[0] + theta[1]*lats))/(theta[4]/2)
+    Z2 = (days - (theta[2] + theta[3]*lats))/(theta[5]/2)
+    P1 = scipy_norm.cdf(Z1)
+    P2 = 1-scipy_norm.cdf(Z2)
+    return np.c_[P1, P2].min(axis=1)
+
+def m_torch(lat, day, theta):
+    theta1, theta2, theta3, theta4, theta5, theta6 = theta
+    term1 = torch.distributions.Normal(0.0, 1.0).cdf((day - (theta1 + theta2 * lat)) / (theta5/2.0))
+    term2 = 1 - torch.distributions.Normal(0.0, 1.0).cdf((day - (theta3 + theta4 * lat)) / (theta6/2.0))
+    return torch.min(term1, term2)
+
+def migration_loss(theta, y, lats, days, threshold, theta_mean, theta_prec, grid_vec):
+    probs = threshold*m_torch(lats, days, theta)
+    probs = torch.clamp(probs, 1e-6, 1 - 1e-6)
+    llh = torch.mean(y*torch.log(probs) + (1 - y)*torch.log(1 - probs)) # easier to tune penalty with average
+    residual = m_torch(grid_vec[:,0], grid_vec[:,1], theta) - m_torch(grid_vec[:,0], grid_vec[:,1], theta_mean)
+    penalty = -theta_prec*torch.mean(residual**2)
+    return -(llh + penalty)
+
+def fit_migration_model(y_train, lat_train, day_train, threshold_train, theta_mean, theta_prec,
+                        lr=0.1, num_epochs=10000, tol=0.0001):
+    y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
+    lat_train_tensor = torch.tensor(lat_train, dtype=torch.float32)
+    day_train_tensor = torch.tensor(day_train, dtype=torch.float32)
+    threshold_train_tensor = torch.tensor(threshold_train, dtype=torch.float32)
+    theta_mean_tensor = torch.tensor(theta_mean, dtype=torch.float32)
+    
+    grid_vec = torch.cartesian_prod(torch.arange(60.0,70.0, 1), torch.arange(1.0,366.0, 10))
+    
+    theta =  torch.tensor(theta_mean_tensor.detach().numpy(), requires_grad=True)
+    optimizer = torch.optim.Adam([theta], lr=lr)
+    losses = []
+    for epoch in tqdm.tqdm(range(num_epochs), desc="Training migration model"):
+        optimizer.zero_grad()
+        loss = migration_loss(theta, y_train_tensor, lat_train_tensor, day_train_tensor, 
+                              threshold_train_tensor, theta_mean_tensor, theta_prec, grid_vec)
+        loss.backward()
+        optimizer.step()
+
+        if (epoch + 1) % 100 == 0 or epoch == 0:
+            losses.append(loss.item())
+
+        if len(losses)>2 and np.abs(losses[-1] - losses[-2])/losses[-2] < tol:
+            break
+    
+    return theta.detach().numpy()
---
+++ single_species/spatial_utils.py
@@ -0,0 +1,125 @@
+import tqdm
+import rasterio
+import numpy as np
+from matplotlib import pyplot as plt
+
+from scipy.optimize import minimize_scalar
+
+def inverse_haversine(lat_center, lon_center, r, R=6371.0):
+    """Get lat/lon of four points r units away from lat/lon center, moving in cardinal directions."""
+    lat_rad_center = np.radians(lat_center)
+    lon_rad_center = np.radians(lon_center)
+    
+    lat_rad_min = lat_rad_center - r/R
+    lat_rad_max = lat_rad_center + r/R
+    lon_rad_min = lon_rad_center + 2*np.arcsin(-np.sin(0.5*r/R)/np.cos(lat_rad_center))
+    lon_rad_max = lon_rad_center + 2*np.arcsin(np.sin(0.5*r/R)/np.cos(lat_rad_center))
+    
+    return np.degrees(lat_rad_min), np.degrees(lat_rad_max), np.degrees(lon_rad_min), np.degrees(lon_rad_max)
+
+def haversine(lat1, lon1, lat2, lon2, R = 6371.0):
+    """Geodesic distance on sphere."""
+    lon_rad1, lat_rad1 = np.radians(lon1), np.radians(lat1)
+    lon_rad2, lat_rad2 = np.radians(lon2), np.radians(lat2)
+    hav = np.sin(0.5*(lat_rad2 - lat_rad1 ))**2 + np.cos(lat_rad1)*np.cos(lat_rad2)*np.sin(0.5*(lon_rad2 - lon_rad1))**2
+    return 2*R*np.arcsin(np.sqrt(hav))
+
+def binary_search_inc(arr, tgt):
+    """Find target in increasing array arr with binary search."""
+    lo, hi = 0, len(arr)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if arr[mid] < tgt:
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo
+
+def binary_search_dec(arr, tgt):
+    """Find target in decreasing array arr with binary search."""
+    lo, hi = 0, len(arr)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if tgt >= arr[mid]:  
+            hi = mid
+        else: lo = mid+1
+    return lo
+
+
+class DistributionMap():
+    def __init__(self, cell_idx, lat_grid, lon_grid, mean_map, var_map, sp_name=None):
+        self.cell_idx = cell_idx
+        self.lat_grid = lat_grid
+        self.lon_grid = lon_grid
+        self.mean_map = mean_map
+        self.var_map = var_map
+        self.sp_name = sp_name
+        
+        self.height = cell_idx.shape[0]
+        self.width = cell_idx.shape[1]
+        self.C = self.height*self.height
+        
+    def cell_to_coord(self, c):
+        # get (lat, lon) of the cell index c
+        return self.lat_grid[c//self.width], self.lon_grid[c%self.width]
+    
+    def calculate_kernel(self, c, cells, r_kernel):
+        c0_coords = self.cell_to_coord(c)
+        lats, lons = self.cell_to_coord(cells)
+        cell_dists = haversine(*c0_coords, lats, lons)
+        cell_weights = np.exp(-0.5*np.square(cell_dists)/r_kernel**2)
+        return cell_weights
+    
+    def get_nearby_cells(self, c, r):
+        # Map cell idx to lat/lon
+        lat_center, lon_center = self.cell_to_coord(c)
+        
+        # get bounding box
+        lat_min, lat_max, lon_min, lon_max = inverse_haversine(lat_center, lon_center, r)
+        
+        # locate lat/long bounds on the regular grid
+        # lat_min_idx > lat_max_idx because lat_grid is decreasing
+        lat_min_idx = binary_search_dec(self.lat_grid, lat_min)
+        lat_max_idx = binary_search_dec(self.lat_grid, lat_max)
+        lon_min_idx = binary_search_inc(self.lon_grid, lon_min)
+        lon_max_idx = binary_search_inc(self.lon_grid, lon_max)
+
+        # select nearby cells, then disregard those without distribution values (e.g., out of country points)
+        neighborhood = self.cell_idx[lat_max_idx:lat_min_idx, lon_min_idx:lon_max_idx]
+        mask = ~np.isnan(self.mean_map[lat_max_idx:lat_min_idx, lon_min_idx:lon_max_idx])
+        
+        return neighborhood[mask]
+        
+class DataMap():
+    def __init__(self, Y_dict, threshold_dict, has_data):
+        self.Y_dict = Y_dict
+        self.threshold_dict = threshold_dict
+        self.has_data = has_data
+        
+    def pool_data(self, cells):
+        Y_train = np.concatenate([self.Y_dict[c] for c in cells])
+        n_train = [len(self.Y_dict[c]) for c in cells]
+        threshold_train = np.concatenate([self.threshold_dict[c] for c in cells])
+        return Y_train, n_train, threshold_train
+    
+def GWR_loss(da, y, threshold, weights, prior_mean, prior_prec):
+    probs = da*threshold
+    probs = np.clip(probs, 1e-6, 1-1e-6)
+    llh = (y*np.log(probs) + (1 - y)*np.log(1 - probs)).dot(weights)
+    penalty = -0.5*prior_prec*(da - prior_mean)**2
+    return -(llh + penalty)
+
+def GWR_prec(post_mean, Y_train, threshold_train, weights_train, prior_prec):
+    num = Y_train - 2*post_mean*Y_train*threshold_train + np.square(threshold_train)*post_mean**2
+    denom = np.square(1-post_mean*threshold_train)*post_mean**2
+    post_prec = prior_prec + weights_train.dot(num/denom)
+    return post_prec
+
+def fit_GWR(Y_train, threshold_train, weights_train, prior_mean, prior_prec):
+    # example model fitting function
+    # not finding variance correctly (e.g., second derivative); just returning prior
+    optim_output = minimize_scalar(GWR_loss, bounds=(0, 1), method='bounded',
+                                   args=(Y_train, threshold_train, weights_train, prior_mean, prior_prec))
+    post_mean = optim_output.x
+    post_prec = GWR_prec(post_mean, Y_train, threshold_train, weights_train, prior_prec)
+    return {"mean":post_mean, "variance":1/post_prec}
