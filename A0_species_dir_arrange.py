e551e59b9c54b784f9b9ca2936718a9386c44bbe
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2025-03-20 16:32:28 +0200

    Add preprocessing of species priors

--- A0_species_dir_arrange.py
+++ A0_species_dir_arrange.py
@@ -1,10 +1,26 @@
 import os
 import tqdm
 import shutil
+import pyreadr
+import pickle
+import pandas as pd
 
 path_project = "/scratch/project_2003104/gtikhono/realtime_birds"
 dir_orig_data = "orig_data"
 dir_data = "data/species"
+
+path_priors = os.path.join(path_project, dir_orig_data, "prior predictions")
+fn = os.listdir(path_priors)
+for f in tqdm.tqdm(fn):
+  x = f.rfind("_")
+  sp = f[:x]
+  sp_new = sp.lower().replace(" ", "_")
+  species_raw = pyreadr.read_r(os.path.join(path_priors, sp+"_prior.RData"))
+  species = pd.concat([species_raw[k] for k in species_raw.keys()], axis=1)
+  species["complete"] = species.isna().sum(axis=1) == 0
+  os.makedirs(os.path.join(path_project, dir_data, sp_new), exist_ok=True)
+  with open(os.path.join(path_project, dir_data, sp_new, sp_new+"_prior.pickle"), "wb") as handle:
+    pickle.dump(species, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 for prefix in ["a", "b", "va"]:
   fn = os.listdir(os.path.join(path_project, dir_orig_data,  "%s_maps"%prefix))
@@ -15,8 +31,8 @@ for prefix in ["a", "b", "va"]:
     os.makedirs(os.path.join(path_project, dir_data, sp_new), exist_ok=True)
     file_orig = os.path.join(path_project, dir_orig_data, "%s_maps"%prefix, "%s_%s.tif"%(sp, prefix))
     if prefix == "va":
-	suffix_new = prefix+"L"
+      suffix_new = prefix+"L"
     else:
-	suffix_new = prefix
-    file_new = os.path.join(path_project, dir_data, sp_new, "%s_%s.tif"%(sp_new, suffix_new))
-    shutil.copyfile(file_orig, file_new)
+      suffix_new = prefix
+      file_new = os.path.join(path_project, dir_data, sp_new, "%s_%s.tif"%(sp_new, suffix_new))
+      shutil.copyfile(file_orig, file_new)
--- A2_prepare_species_data.py
+++ A2_prepare_species_data.py
@@ -24,13 +24,6 @@ sp_list = os.listdir(path_data)
 sp_list.sort()
 sp_dir = sp_list[args.species_id]
 
-species_raw = pyreadr.read_r(sp_dir + sp + "_prior.RData")
-species = pd.concat([species_raw[k] for k in species_raw.keys()], axis=1)
-species["complete"] = species.isna().sum(axis=1) == 0
-
-with open(sp_dir + sp_lower + "_prior.pickle", "wb") as handle:
-    pickle.dump(species, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
 print("Calculating vaL for id %d, species %s" % (sp_id, sp_dir))
 with rasterio.open(os.path.join(path_data, sp_dir, sp_dir+"_a.tif")) as src:
     a_map, profile = src.read(1), src.profile
