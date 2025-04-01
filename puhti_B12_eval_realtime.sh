06f20d367fd709dd8ca84a341ff4e6fbf93038d1
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2025-04-01 17:24:47 +0300

    Adjust puhti_B12 script for fitting with keeping migration prior

--- puhti_B12_eval_realtime.sh
+++ puhti_B12_eval_realtime.sh
@@ -4,18 +4,19 @@
 #SBATCH --output=output/%A_%a
 #SBATCH --ntasks=1 --cpus-per-task=4
 #SBATCH --mem=20G
-#SBATCH --time=22:15:00 --partition=small
-#SBATCH --array=40-262
+#SBATCH --time=32:15:00 --partition=small
+#SBATCH --array=0-262
 
 IND=$SLURM_ARRAY_TASK_ID
 STEP=1
 D0=${1:-1}
 D1=${2:-365}
-SNP=${3:-0}
-SPR=${4:-1}
-SI=${5:-1}
-F=${6:-10}
-JN=${7:-4}
+MIGTRAIN=${3:-1}
+SNP=${4:-0}
+SPR=${5:-1}
+SI=${6:-0}
+F=${7:-10}
+JN=${8:-4}
 
 
 mkdir -p output
@@ -23,15 +24,22 @@ module load pytorch
 module load hyperqueue
 hostname
 
-for i in {1..365}
+for i in {0..365}
 do
-M0=366
-M1=$((365+i*STEP))
+if [[ $MIGTRAIN == 1 ]]; then
+  M0=366
+  M1=$((365+i*STEP))
+  echo "Training migration model"
+elif [[ $MIGTRAIN == 0 ]]; then
+  M0=0
+  M1=0
+  echo "Not training migration model"
+fi
 S0=366
 S1=$((365+i*STEP))
 T0=$((365+i*STEP+1))
 T1=$((365+i*STEP+7))
-if ! (( $i % 7 )) ; then continue; fi
+if (( $i % 1 )) ; then continue; fi
 srun python3 B1_eval_species.py $IND --detstart $D0 --detstop $D1 --migstart $M0 --migstop $M1 \
   --spatstart $S0 --spatstop $S1 --teststart $T0 --teststop $T1 --savenewprior $SNP --savepred $SPR \
   --saveimages $SI  --factor $F --jn $JN
